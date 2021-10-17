dbd65984b932596bf77fdcf023b5d4ed8d6b3145
Robert Gutiérrez <jrgutinez@gmail.com>
2021-10-17 21:33:23 +0200

    var updates

---
+++ Unidad4/src/Extras/Racional.java
@@ -0,0 +1,168 @@
+package Extras;
+
+import java.util.Scanner;
+
+public class Racional {
+	/**
+	 * Realizar un programa en Java que pida numerador y denominador para realizar
+	 * operaciones con fracciones y además pueda simplificarlas.
+	 * 
+	 * @author Robert G
+	 */
+	private int num; // numerador
+	private int den; // denominador
+
+	// Método simplificar fracciones
+	public void simplifica() {
+
+		int limite;
+		if (this.num > this.den)
+			limite = this.num;
+		else
+			limite = this.den;
+
+		int i = (int) limite / 2;
+
+		// System.out.println("i vale "+i);
+		while (i > 0) {
+
+			if (this.num % i == 0 && this.den % i == 0) {
+				this.num = this.num / i;
+				this.den = this.den / i;
+			}
+			i--;
+		}
+	}
+
+	public Racional() {
+		// TODO Auto-generated constructor stub
+		this.num = 1;
+		this.den = 1;
+	}
+
+	public Racional(int num, int den) {
+		this.num = num;
+		if (den != 0)
+			this.den = den;
+		else
+			System.out.println(">>>ERROR El número no es correcto");
+	}
+
+	public String toString() {
+		return (num + "/" + den);
+	}
+
+	public void imprimir() {
+		System.out.println("El número es: " + this.toString());
+	}
+
+	public void impri_prod() {
+		System.out.println("El producto de las fracciones es: " + this.toString());
+	}
+
+	public void impri_div() {
+		System.out.println("El cociente de las fracciones es: " + this.toString());
+	}
+
+	public void impri_sum() {
+		System.out.println("La suma de las fracciones es: " + this.toString());
+	}
+
+	public void impri_res() {
+		System.out.println("La resta de las fracciones es: " + this.toString());
+	}
+
+	// Método para multiplicar fracciones
+	public Racional producto(Racional a) {
+
+		int n = this.num * a.num;
+		int d = this.den * a.den;
+
+		Racional r = new Racional(n, d);
+		r.simplifica();
+		return r;
+	}
+
+	// Método para dividir fracciones
+	public Racional division(Racional a) {
+
+		int n = this.num * a.den;
+		int d = this.den * a.num;
+
+		Racional r = new Racional(n, d);
+		r.simplifica();
+		return r;
+	}
+
+	// Método para sumar fracciones
+	public Racional sumar(Racional a) {
+		int n, d;
+		if (this.den == a.den) {
+			n = this.num + a.num;
+			d = a.den;
+
+		} else {
+			n = this.num * a.den + this.den * a.num;
+			d = this.den * a.den;
+		}
+
+		Racional r = new Racional(n, d);
+		r.simplifica();
+		return r;
+
+	}
+
+	// Método para restar fracciones
+	public Racional restar(Racional a) {
+		int n, d;
+		if (this.den == a.den) {
+			n = this.num - a.num;
+			d = a.den;
+
+		} else {
+			n = this.num * a.den - this.den * a.num;
+			d = this.den * a.den;
+		}
+
+		Racional r = new Racional(n, d);
+		r.simplifica();
+		return r;
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce primer numerador: ");
+		int n = sc.nextInt();
+		System.out.println("Introduce primer denominador: ");
+		int d = sc.nextInt();
+
+		Racional r1 = new Racional(n, d);
+		r1.imprimir();
+
+		System.out.println("Introduce segundo numerador: ");
+		n = sc.nextInt();
+		System.out.println("Introduce segundo denominador: ");
+		d = sc.nextInt();
+
+		sc.close();
+
+		Racional r2 = new Racional(n, d);
+		r2.imprimir();
+
+		// Racional r1=new Racional(1,3);
+		// r1.imprimir();
+		// Racional r2=new Racional(1,5);
+		// r2.imprimir();
+		Racional r3 = (r1.producto(r2));
+		r3.impri_prod();
+		Racional r4 = (r1.division(r2));
+		r4.impri_div();
+		Racional r5 = (r1.sumar(r2));
+		r5.impri_sum();
+		Racional r6 = (r1.restar(r2));
+		r6.impri_res();
+	}
+}
--- Unidad4/src/claseAbstracta/Main.java
+++ Unidad4/src/claseAbstracta/Main.java
@@ -12,5 +12,26 @@ public class Main {
 		Cuadrado c2 = new Cuadrado(0, 0, 3);
 		System.out.println("El área del círculo es: " + c.area());
 		System.out.println("El área del cuadrado es: " + c2.area());
+
+		// Ejecuta los métodos de finalización de cualquier objeto pendiente de finalización.
+		System.runFinalization();
+		// Ejecuta el recolector de basura.
+		System.gc(); // Garbage Collector
 	}
 }
+
+/**
+ * Ejecuta los métodos de finalización de cualquier objeto pendiente de finalización.
+ * Llamar a este método sugiere que la máquina virtual Java se esfuerce por ejecutar
+ * los métodos de finalización de los objetos que se han encontrado descartados pero
+ * cuyos métodos de finalización aún no se han ejecutado. Cuando el control regresa
+ * de la llamada al método, Java Virtual Machine ha hecho un gran esfuerzo para
+ * completar todas las finalizaciones pendientes.
+ * 
+ * Ejecuta el recolector de basura. Llamar al método gc sugiere que Java
+ * VirtualMachine se esfuerza por reciclar los objetos no utilizados para que la
+ * memoria que ocupan actualmente esté disponible para su rápida reutilización.
+ * Cuando el control regresa de la llamada al método, Java VirtualMachine ha hecho un
+ * gran esfuerzo para recuperar espacio de todos los objetos descartados.
+ * 
+ */
